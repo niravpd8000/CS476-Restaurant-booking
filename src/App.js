b9f9796d3ec51b164e2e041019e22469cccc60df
Nirav Dhameliya <dhameliyanirav8000@gmail.com>
2023-10-24 19:22:35 -0600

    SRC directory wrong path issue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import routes from "./routes";
+import Header from "./containers/Header/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="c-app c-default-layout">
+                <div className="c-wrapper">
+                    <div className="layout-display-center">
+                        <Header/>
+                        <div style={{marginTop: "90px"}}></div>
+                        <Routes>
+                            {
+                                routes.map((route, idx) => {
+                                    return route.component && (
+                                        <Route
+                                            key={idx}
+                                            path={route.path}
+                                            element={route.component}
+                                        />
+                                    )
+                                })
+                            }
+                        </Routes>
+                    </div>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
