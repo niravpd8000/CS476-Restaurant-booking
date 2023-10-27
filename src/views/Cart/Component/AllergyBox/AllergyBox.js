235b51547da5ca493a832bf9e9b6345845e0d61a
unknown <euharajv@uregina.ca>
2023-10-27 14:10:07 -0600

    Cart Part1

--- src/views/Cart/Component/AllergyBox/AllergyBox.js
+++ src/views/Cart/Component/AllergyBox/AllergyBox.js
@@ -1,14 +1,30 @@
-import React from "react";
-import classes from "./AllergyBox.module.css";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { TextField } from "@mui/material";
 
-const AllergyBox = () => {
-  // Logic for allergy box component
+const AllergyBox = ({ allergies }) => {
+    const [allergyInput, setAllergyInput] = useState('');
 
-  return (
-    <div className={classes.allergyBox}>
-      {/* Content of allergy box */}
-    </div>
-  );
+    const handleInputChange = (event) => {
+        setAllergyInput(event.target.value);
+    };
+
+    return (
+        <div>
+            <TextField
+                fullWidth
+                label="Enter Allergy Information"
+                variant="outlined"
+                value={allergyInput}
+                onChange={handleInputChange}
+            />
+            {/* Display other allergy-related components or logic as needed */}
+        </div>
+    );
+};
+
+AllergyBox.propTypes = {
+    allergies: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default AllergyBox;
--- src/views/Cart/index.js
+++ src/views/Cart/index.js
@@ -1,22 +1,39 @@
 import React from 'react';
-import { Grid, Button } from "@mui/material";
+import { Grid, Button, Typography, TextField } from "@mui/material";
 import CartSummary from "./Component/CartSummary/CartSummary";
+import AllergyBox from "./Component/AllergyBox/AllergyBox";
+
+const allergies = ["Peanuts", "Gluten", "Dairy"]; // Example list of allergies
 
 function MainCart() {
     return (
         <main>
             <CartSummary/>
             <Grid container spacing={2}>
+                {/* Product Information Section */}
                 <Grid item xs={12} sm={6}>
-                    {/* You can add content specific to your cart item here */}
+                    <Typography variant="h5" gutterBottom>
+                        Product Information
+                    </Typography>
+                    {/* Input Bar for Product Information */}
+                    <TextField fullWidth label="Product Information" variant="outlined" />
                 </Grid>
+
+                {/* Allergy Information Section */}
                 <Grid item xs={12} sm={6}>
-                    {/* You can add another grid item for cart item or any other content */}
+                    <Typography variant="h5" gutterBottom>
+                        Allergy Information
+                    </Typography>
+                    {/* Input Bar for Allergy Information */}
+                    <AllergyBox allergies={allergies} />
+                </Grid>
+
+                {/* Button placed at the bottom */}
+                <Grid item xs={12}>
                     <Button variant="contained" color="primary">
                         Place Order
                     </Button>
                 </Grid>
-                {/* Add more Grid items as needed */}
             </Grid>
         </main>
     );
