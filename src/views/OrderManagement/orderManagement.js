2f647dd10727c972e1649b0385431f66b1902a4f
xuanning fan <fanharold@gmail.com>
2023-11-04 20:27:40 -0600

    Order managing changes

--- src/views/OrderManagement/orderManagement.js
+++ src/views/OrderManagement/orderManagement.js
@@ -1,55 +1,79 @@
-import React  from 'react';
-import Grid from '@mui/material/Grid';
-import { Typography,} from '@mui/material';
-// import './orderManagement.css';
-
-
+import React from 'react';
+import { Button, Paper, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
 
 function OrderManagement() {
-    return ( <>
-    
-    <grid container>
+  return (
+    <div style={{ padding: 20 }}>
+      <Paper elevation={3} style={{ padding: 20, marginBottom: 20 }}>
+        <h3>Order Management</h3>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Order Number</TableCell>
+              <TableCell>Customer Name</TableCell>
+              <TableCell>Estimate time to pick up</TableCell>
+              <TableCell style={{ width: 120 }}></TableCell> {/* Set width /}
+              <TableCell style={{ width: 120 }}></TableCell> {/ Set width */}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Array(3)
+              .fill(null)
+              .map((_, index) => (
+                <TableRow key={index}>
+                  <TableCell>#abcd123</TableCell>
+                  <TableCell>ABCD XYZ</TableCell>
+                  <TableCell>10:15pm</TableCell>
+                  <TableCell>
+                    <Button variant="contained" color="success">
+                      Accept
+                    </Button>
+                  </TableCell>
+                  <TableCell>
+                    <Button variant="outlined" color="primary">
+                      Decline
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </Paper>
 
-<grid item xs={12}>
-        <div className='div1'>
-            <span>Order Number</span>
-            <span>Customer Name</span>
-            <span>Estimate time to pick up </span>
-        </div>
-</grid>
+      <Paper elevation={3} style={{ padding: 20, marginBottom: 20 }}>
+        <Table>
+        <TableHead>
+            <TableRow>
+              <TableCell>Order Number</TableCell>
+              <TableCell>Customer Name</TableCell>
+              <TableCell>Estimate time to pick up</TableCell>
+              <TableCell style={{ width: 120 }}></TableCell> {/* Set width */}
+              <TableCell style={{ width: 120 }}></TableCell> {/* Set width */}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Array(2)
+              .fill(null)
+              .map((_, index) => (
+                <TableRow key={index}>
+                  <TableCell>#abcd123</TableCell>
+                  <TableCell>1000000000 </TableCell>
+                  <TableCell>10:15pm</TableCell>
+                  <TableCell>
+                    <Button variant="contained" color="warning">
+                      View
+                    </Button>
+                  </TableCell>
+                  <TableCell></TableCell> {/* Empty TableCell for alignment */}
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </Paper>
 
-    <grid item xs={8}  md={8} >
-        <div className='div2'>
-            <span>#someId</span>
-            <span>Name</span>
-            <span>20:00pm</span>
-            <span>
-                <button className='button1'>Accept</button>
-                <button className='button2'>decline</button>
-            </span>
-        </div>
-    
-     </grid>
-
-
-    <grid item xs={8}  md={8}> 
-    <div className='div2'>
-    <span>#someId</span>
-            <span>Name</span>
-            <span> Picked up</span>
-            <span>
-                <button  className='button3' >view</button>
-            </span>
 
     </div>
-    </grid>
-    </grid>
-    
-    
-    
-    
-    
-    </> );
+  );
 }
 
 export default OrderManagement;
